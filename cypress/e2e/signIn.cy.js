e35f881d9839643038ea860e3102e6db24cac714
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-13 14:53:31 +0200

    Dodano zmienne

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,10 +1,16 @@
 /// <reference types="cypress" />
 
+// Ignoruj nieobsłużone wyjątki po stronie aplikacji
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false;
+});
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
     const primaryUrl = 'https://react-redux.realworld.io';
     const backupUrl = 'https://conduit.mate.academy';
 
+    // Funkcja odwiedzająca stronę logowania z fallbackiem
     function visitLogin(url) {
       cy.visit(`${url}/#/login`, { failOnStatusCode: false }).then(($resp) => {
         if ($resp.status === 404 && url === primaryUrl) {
@@ -15,12 +21,16 @@ describe('Sign In page', () => {
 
     visitLogin(primaryUrl);
 
+    // Wypełnij pola danymi z Cypress.env
     cy.get('input[type="email"]').type(Cypress.env('USER_EMAIL'));
     cy.get('input[type="password"]').type(Cypress.env('USER_PASSWORD'));
 
+    // Kliknij przycisk Sign in
     cy.contains('button', 'Sign in').click();
 
+    // Sprawdź, czy username pojawił się w nagłówku
     cy.get('nav').contains('a', Cypress.env('USER_NAME')).should('be.visible');
   });
 });
+
 

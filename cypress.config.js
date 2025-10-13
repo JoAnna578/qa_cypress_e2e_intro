f468618bcd93ed8f0d327e84d47fae8d162b4d39
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-13 14:49:43 +0200

    Automatyczne logowanie w aplikacji RealWorld
    Test Cypress weryfikuje możliwość zalogowania się do aplikacji przy użyciu zmiennych środowiskowych dla e-maila, hasła i nazwy użytkownika. Obsługuje zarówno podstawowy, jak i zapasowy adres URL oraz sprawdza, czy użytkownik pojawia się w nagłówku po zalogowaniu.

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,14 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://react-redux.realworld.io', // podstawowa strona
     setupNodeEvents(on, config) {
+      // Możesz tu zostawić pustą funkcję lub dodać hooki
+    },
+    env: {
+      USER_EMAIL: 'joannakoloczek1@gmail.com',
+      USER_PASSWORD: 'Szczurojeb57!',
+      USER_NAME: 'Jo'
     }
   }
 });

69922366be29ce8ddbf10ba8d33a0110052712c1
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-13 14:18:36 +0200

    Automatyzacja logowania w aplikacji Conduit
    Test w pełni automatyzuje proces logowania: odwiedzenie strony, wpisanie poprawnych danych, kliknięcie przycisku Sign In oraz weryfikację, że nazwa użytkownika pojawia się w nagłówku. Sprawdza podstawowy flow uwierzytelniania użytkownika.

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,6 +2,17 @@
 
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    // 1. Odwiedzenie strony logowania
+    cy.visit('https://react-redux.realworld.io/#/login');
 
+    // 2. Wpisanie poprawnego e-maila i hasła
+    cy.get('input[type="email"]').type('twój_email@example.com');
+    cy.get('input[type="password"]').type('TwojeHaslo123');
+
+    // 3. Kliknięcie przycisku [Sign In]
+    cy.get('button[type="submit"]').click();
+
+    // 4. Sprawdzenie, czy nazwa użytkownika pojawiła się w nagłówku
+    cy.get('nav').contains('TwojaNazwaUzytkownika').should('exist');
   });
 });

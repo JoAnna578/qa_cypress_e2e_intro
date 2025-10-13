cc4930b29fe87f267cd38d11e376b0085965dc50
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-13 14:58:10 +0200

    Finalna zmiana

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,19 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
-    baseUrl: 'https://react-redux.realworld.io',
+    baseUrl: 'https://react-redux.realworld.io', // główny URL
+    setupNodeEvents(on, config) {
+      // Tutaj można dodać dodatkowe hooki node
+      return config;
+    },
     env: {
       USER_EMAIL: 'joannakoloczek1@gmail.com',
       USER_PASSWORD: 'Szczurojeb57!',
-      USER_NAME: 'Jo'
-    }
-  }
+      USER_NAME: 'Jo',
+      BACKUP_URL: 'https://conduit.mate.academy'
+    },
+    defaultCommandTimeout: 10000, // zwiększony timeout dla dynamicznie ładowanych elementów
+  },
 });
+
 

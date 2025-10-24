30b357bc4dbae5f3df73eb382b073849aa2c931e
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-24 21:52:23 +0200

    fix: update USER_NAME and simplify login test using baseUrl

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,41 +1,17 @@
-/// <reference types="cypress" />
+describe('Sign In', () => {
+  beforeEach(() => {
+    // Korzystamy z baseUrl ustawionego w cypress.config.js
+    cy.visit('/#/login');
+  });
 
-// Ignoruj nieobsłużone wyjątki po stronie aplikacji
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false;
-});
-
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
-    const primaryUrl = 'https://react-redux.realworld.io';
-    const backupUrl = 'https://conduit.mate.academy';
-
-    // Funkcja odwiedzająca stronę logowania z fallbackiem
-    function visitLogin(url) {
-      cy.visit(`${url}/#/login`, { failOnStatusCode: false }).then(($resp) => {
-        if ($resp.status === 404 && url === primaryUrl) {
-          visitLogin(backupUrl);
-        }
-      });
-    }
-
-    visitLogin(primaryUrl);
-
-    // Wypełnij pola danymi z Cypress.env
+  it('should log in successfully and display the correct username', () => {
+    // Wprowadzamy dane użytkownika z konfiguracji
     cy.get('input[type="email"]').type(Cypress.env('USER_EMAIL'));
     cy.get('input[type="password"]').type(Cypress.env('USER_PASSWORD'));
+    cy.get('button[type="submit"]').click();
 
-    // Przechwytujemy request logowania, aby poczekać na odpowiedź
-    cy.intercept('POST', '**/users/login').as('loginRequest');
-
-    // Kliknij przycisk Sign in
-    cy.contains('button', 'Sign in').click();
-
-    // Czekaj na zakończenie requestu logowania
-    cy.wait('@loginRequest');
-
-    // Sprawdź, czy username pojawił się w nagłówku po zalogowaniu
-    cy.get('nav').contains('a', Cypress.env('USER_NAME'), { timeout: 10000 }).should('be.visible');
+    // Sprawdzamy, czy w nagłówku pojawia się poprawna nazwa użytkownika
+    cy.get('a.nav-link').contains(Cypress.env('USER_NAME')).should('be.visible');
   });
 });
 
